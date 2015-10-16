41dccc3ba688e4c6e1b2748b2d4217c7eaa83c39
john <john@kodalagom.se>
2015-10-16 14:40:33 +0200

    Smooth scroll working

---
+++ DPY3/Scripts/script.js
@@ -0,0 +1,10 @@
+﻿/// <reference path="jquery-2.1.4.js" />
+/// <reference path="scrollto.js" />
+/// <reference path="bootstrap.js" />
+
+$('#mainMenu li a').click(function (event) {
+    event.preventDefault();
+    $('html, body').animate({
+        scrollTop: $($(this).attr('href')).offset().top
+    }, 1000);
+});

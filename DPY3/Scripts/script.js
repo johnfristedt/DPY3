7413902dcc2e306cec1d5c7b25b6024585c623bd
john <john@kodalagom.se>
2015-11-01 12:48:17 +0100

    Stuff

--- DPY3/Scripts/script.js
+++ DPY3/Scripts/script.js
@@ -8,13 +8,15 @@ var pageOffset = -100; // Pixels
 var scrollTime = 3000; // Milliseconds
 var satelliteOffset = 2000; // Pixels
 var satelliteSpeed = 0.5;  // Multiplier
-var backgroundParallaxSpeed = 0.5;  // Multiplier
+var backgroundParallaxSpeed = 0.7;  // Multiplier
+
+//$('body').css('background-size', backgroundParallaxSpeed * 100 + '%');
 
 $('#mainMenu li a, .navbar-brand').click(function (event) {
     event.preventDefault();
     $('html, body').animate({
         scrollTop: $($(this).attr('href')).offset().top + pageOffset
-    }, scrollSpeed);
+    }, scrollTime);
 });
 
 $(document).scroll(function () {
@@ -24,10 +26,18 @@ $(document).scroll(function () {
 });
 
 angular.module('DPY3.SiteCtrl', []).controller('siteCtrl', ['$scope', '$http', function ($scope, $http) {
-    $scope.new = {
-        cloud: {}
+    $scope.clouds = new Array();
+
+    $scope.models = {
+        cloud: {
+            top: 0,
+            left: 0,
+            zIndex: 0
+        }
     };
 
     for (var i = 0; i < 6; i++)
-        $scope.clouds.push($scope.new.cloud);
+        $scope.clouds.push($.extend({}, $scope.models.cloud));
+
+    console.log($scope.clouds);
 }]);
